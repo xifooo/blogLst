fd7c9cee95270190acb00eac0bd294427344223c
xifooo <82203432+xifooo@users.noreply.github.com>
2023-03-12 20:22:01 +0800

    第一次提交 (#1)
    Co-authored-by: xifooo <xifooo@tk2.com>

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const app = express()
+
+const cors = require("cors")
+
+const blogRouter = require("./controllers/blogs_routers")
+const middleware = require("./utils/middlewares")
+const logger = require("./utils/logger")
+const mongoose = require("mongoose")
+const path = require("path")
+
+logger.info(`connecting to ${config.MONGODB_URI}`)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB")
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB", error.message)
+  });
+
+  
+// loading middlewares
+app.use(cors())
+app.use(express.static(path.join(".", "client", "build")))
+app.use(middleware.requestLogger)
+
+app.use("/api/blogs", blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
