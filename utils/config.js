ff105037ae9df8280ecc120890c8bac6d2f061b5
xifooo <82203432+xifooo@users.noreply.github.com>
2023-05-09 10:40:07 +0800

    backup (#4)
    Co-authored-by: jyeeho <xifoo@gmail.com>

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,12 @@
-require('dotenv').config()
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') })
 
-const MONGODB_URI = process.env.MONGODB_URI
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
+// const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
+
 
 module.exports = {
   MONGODB_URI, 
