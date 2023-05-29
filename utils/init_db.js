72d309cf56fc4196ccb69e25a16534a0b298ecf1
xifooo <82203432+xifooo@users.noreply.github.com>
2023-05-29 11:04:17 +0800

    完成Exercise 4.15-4.19 (#6)
    Co-authored-by: xifooo <xifooo@gmail.com>

---
+++ utils/init_db.js
@@ -0,0 +1,43 @@
+const initialUsers = [
+  {
+    username: 'mlonokai',
+    name: "jaks",
+    password: "123123"
+  },
+  {
+    username: 'hellter',
+    name: "jyeho",
+    password: "141414"
+  }
+]
+
+const initialBlogs = [
+  {
+    title: "Under pressure",
+    author: "david bovi",
+    url: "www.baidu.com",
+    likes: 32
+  },
+  {
+    title: "Get it by your hands",
+    author: "chars",
+    url: "youtube.com",
+    likes: 16
+  },
+  {
+    title: "Good times bad times",
+    author: "Zeed",
+    url: "spotify.com",
+    likes: 5
+  },
+  {
+    title: "Smell like teen spirit",
+    author: "Nirvana",
+    url: "junglerocknroll.com",
+    likes: 100
+  }
+]
+
+module.exports = {
+  initialUsers, initialBlogs
+}
